0c1e8379da5e32b581e49b8729cbf5afbcafc6f1
Double <1009083442@qq.com>
2020-07-12 09:45:30 +0800

    24 | Spring IoC依赖来源：依赖注入和查找的对象来自于哪里？

--- ioc-container-overview/src/main/java/org/dbl/study/thinking/in/spring/dependency/injection/DependencyInjectionDemo.java
+++ ioc-container-overview/src/main/java/org/dbl/study/thinking/in/spring/dependency/injection/DependencyInjectionDemo.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * ClassName: DependencyLookupDemo <br>
@@ -27,14 +28,15 @@ public class DependencyInjectionDemo {
     BeanFactory beanFactory =
         new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
 
+    //    依赖来源一：自定义 Bean
     UserRepository userRepository = beanFactory.getBean("userRepository", UserRepository.class);
 
     //    System.out.println(userRepository.getUsers());
-    //    依赖注入
+    //    依赖来源二：依赖注入（内建依赖）
     System.out.println(userRepository.getBeanFactory());
     //    System.out.println(userRepository.getBeanFactory() == beanFactory);
 
-    //    依赖查找
+    //    依赖查找（错误）
     //    System.out.println(beanFactory.getBean(BeanFactory.class));
 
     //    ObjectFactory<User> userObjectFactory = userRepository.getUserObjectFactory();
@@ -45,5 +47,9 @@ public class DependencyInjectionDemo {
     System.out.println(contextObjectFactory.getObject());
 
     System.out.println(beanFactory == contextObjectFactory.getObject());
+
+    //    依赖来源三： 容器内建 Bean
+    Environment environment = beanFactory.getBean(Environment.class);
+    System.out.println("获取 Environment 类型的 Bean:" + environment);
   }
 }
