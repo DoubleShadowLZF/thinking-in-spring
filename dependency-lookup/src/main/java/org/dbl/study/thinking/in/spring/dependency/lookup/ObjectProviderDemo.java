9f7f6670d198f37c6360398b060b5bd39d90092f
Double <1009083442@qq.com>
2020-07-15 00:30:19 +0800

    46 | 延迟依赖查找：非延迟初始化Bean也能实现延迟查找？

--- dependency-lookup/src/main/java/org/dbl/study/thinking/in/spring/dependency/lookup/ObjectProviderDemo.java
+++ dependency-lookup/src/main/java/org/dbl/study/thinking/in/spring/dependency/lookup/ObjectProviderDemo.java
@@ -1,8 +1,10 @@
 package org.dbl.study.thinking.in.spring.dependency.lookup;
 
+import org.dbl.study.thinking.in.spring.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 /**
  * ClassName: ObjectProviderDemo <br>
@@ -28,13 +30,39 @@ public class ObjectProviderDemo { // @Configuration 是非必须的注解
     //    依赖查找集合对象
     lookupByObjectProvider(applicationContext);
 
+    lookupIfAvailable(applicationContext);
+
+    lookupByStreamOps(applicationContext);
+
     //    显示关闭 Spring 应用上下文
     applicationContext.close();
   }
 
+  private static void lookupByStreamOps(AnnotationConfigApplicationContext applicationContext) {
+    ObjectProvider<String> beanProvider = applicationContext.getBeanProvider(String.class);
+    //    for (String str : beanProvider) {
+    //      System.out.println(str);
+    //    }
+
+    // Stream -> Method reference
+    beanProvider.stream().forEach(System.out::println);
+  }
+
+  private static void lookupIfAvailable(AnnotationConfigApplicationContext applicationContext) {
+    ObjectProvider<User> userObjectProvider = applicationContext.getBeanProvider(User.class);
+    User user = userObjectProvider.getIfAvailable(User::createUser);
+    System.out.println("当前 User 对象 ： " + user);
+  }
+
   @Bean
+  @Primary
   public String helloWorld() { // 方法名就是 Bean 名称 = "helloWorld"
     return "hello,world";
+  }
+
+  @Bean
+  public String message() {
+    return "message";
   }
 
   private static void lookupByObjectProvider(
