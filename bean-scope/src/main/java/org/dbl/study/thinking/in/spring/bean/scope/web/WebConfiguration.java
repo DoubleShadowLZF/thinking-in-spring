7c1a540784d47415089b0df9d7e6d8918c35091c
Double <1009083442@qq.com>
2020-07-23 19:31:09 +0800

    83 | "session" Bean作用域：session Bean在Spring MVC场景下存在哪些局限性？

--- bean-scope/src/main/java/org/dbl/study/thinking/in/spring/bean/scope/web/WebConfiguration.java
+++ bean-scope/src/main/java/org/dbl/study/thinking/in/spring/bean/scope/web/WebConfiguration.java
@@ -3,7 +3,7 @@ package org.dbl.study.thinking.in.spring.bean.scope.web;
 import org.dbl.study.thinking.in.spring.domain.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -18,7 +18,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 public class WebConfiguration {
   @Bean
-  @RequestScope
+  //  @RequestScope
+  @SessionScope
   public User user() {
     User user = User.createUser();
     System.out.println("User Bean 注入 Spring IOC ： " + user.hashCode());
